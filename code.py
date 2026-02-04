671a9a1cac71e90627aa2b862f9d143e50618d0a
Mike Shon <michael_shon@yahoo.com>
2026-02-04 15:48:05 -0500

    Update code.py to add https request with ada connection mgr
    connection manager snippet from 
    https://learn.adafruit.com/networking-in-circuitpython/networking-with-the-wifi-module

--- code.py
+++ code.py
@@ -8,4 +8,17 @@ print("connecting... or maybe already connected....")
 #                  password=os.getenv('CIRCUITPY_WIFI_PASSWORD'))
 print("my IP addr:", wifi.radio.ipv4_address)
 
-print("hello, world")
+import adafruit_connection_manager
+import adafruit_requests
+
+radio = wifi.radio
+
+# Add code to make sure your radio is connected
+
+pool = adafruit_connection_manager.get_radio_socketpool(radio)
+ssl_context = adafruit_connection_manager.get_radio_ssl_context(radio)
+requests = adafruit_requests.Session(pool, ssl_context)
+response = requests.get("http://wifitest.adafruit.com/testwifi/index.html")
+
+# Do something with response
+print(response)
