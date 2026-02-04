3ba23c85a3274efe8a826b509c11194c1fca5de8
Mike Shon <michael_shon@yahoo.com>
2026-02-04 15:19:09 -0500

    Create code.py to print wifi IP

---
+++ code.py
@@ -0,0 +1,11 @@
+# code.py
+import os, wifi
+print("SSID: " + os.getenv('CIRCUITPY_WIFI_SSID') )
+print("Password: " + os.getenv('CIRCUITPY_WIFI_PASSWORD') )
+
+print("connecting... or maybe already connected....")
+# wifi.radio.connect(ssid=os.getenv('CIRCUITPY_WIFI_SSID'),
+#                  password=os.getenv('CIRCUITPY_WIFI_PASSWORD'))
+print("my IP addr:", wifi.radio.ipv4_address)
+
+print("all done")
