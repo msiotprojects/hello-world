2252076a948143533c08fcc006c087d275a0b920
Mike Shon <michael_shon@yahoo.com>
2026-02-28 15:05:31 -0500

    Update code.py - change message after reading button

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
-
-
-# Based on MicroPython OTAUpdater, 
-# connectToWifiAndUpdate() below assumes that immediatly on reset, 
+# Based on MicroPython OTAUpdater at GitHub rdehuyss/micropython-ota-updater
+#
+# connectToWifiAndUpdate() below assumes that immediatly on reset,
 # we want to connect to wifi,  check for update, and install it.
 #      CircuitPython will auto-connect to wifi on restart IF
 #      you have CIRCUITPY_WIFI_SSID and CIRCUITPY_WIFI_PASSWORD set up
@@ -18,7 +17,7 @@ def connectToWifiAndUpdate():
 
     from app.ota_updater import OTAUpdater
 
-    
+
 
     print('Memory free', gc.mem_free())
     # If desired, you can pass the GitHub repo and other parameters here,
@@ -27,11 +26,11 @@ def connectToWifiAndUpdate():
     settings = OTAUpdater.get_misc_settings()
 
         # initialize network if not already active
-    OTAupdate._using_network(settings["wifi_ssid"], settings["wifi_password"))
-    
+    OTAUpdater._using_network(settings["wifi_ssid"], settings["wifi_password"])
+
     # Micropython OTAUpdater passed ( 'https://github.com/owner_name/repo_name', main_dir='app', secrets_file="secrets.py")
-    otaUpdater = OTAUpdater(settings = settings, main_dir='tst')
-    
+    otaUpdater = OTAUpdater(settings = settings, github_repo = None)
+
     hasUpdated = otaUpdater.install_update_if_available()
     if hasUpdated:
         machine.reset()
@@ -41,6 +40,44 @@ def connectToWifiAndUpdate():
 
 def startApp():
     import app.start        # sorry - this assumes that most code is in app/ subdir
+
+
+####################################################
+import board
+import digitalio
+import storage
+from time import sleep
+
+switchD2 = digitalio.DigitalInOut(board.D2)
+switchD2.direction = digitalio.Direction.INPUT
+switchD2.pull = digitalio.Pull.DOWN # turn off internal pull-up resistor
+# None gives  TFFF or TTTT(pressed)
+# DOWN gives  FFFF or TTTT (pressed)
+# UP   gives  TTTT or TTTT
+
+# On Adafruit ESP32-S3 Reverse TFT, pressed gives True
+# and readonly=True means CP cannot write the drive, but the host computer can.
+# By default, the button value will be false,
+#  and CP can update files on the drive via OTA
+print("checking D2:")
+print(switchD2.value)
+sleep(0.1)
+print(switchD2.value)
+sleep(0.1)
+print(switchD2.value)
+sleep(0.1)
+print(switchD2.value)
+
+
+if (switchD2.value) :
+  print("D2 is pressed, host probably in charge")
+#  storage.remount("/", readonly=True)
+else :
+  print("D2 NOT pressed, App code probaby in charge")
+#  storage.remount("/", readonly=False)
+sleep(5)
+#exit
+#######################################################
 
 
 connectToWifiAndUpdate()
