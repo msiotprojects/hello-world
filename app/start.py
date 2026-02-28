c2d24788aef580db73034e3f31a067bbbae088b1
Mike Shon <michael_shon@yahoo.com>
2026-02-28 16:03:13 -0500

    Update start.py - import app.subdir.subdir
    figuring out how lower level import works

--- app/start.py
+++ app/start.py
@@ -2,9 +2,12 @@
 print("Hello World!")
 
 import os
+from time import sleep
+
 f = open("app/.version")
 version = f.read()
-print("version is " + version)
+print("version file contains: " + version)
+sleep(5)
 
-import subdir.subdir
+import app.subdir.subdir
 print("subdir contains : " + subdir.subdir_var) 
