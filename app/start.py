0b619f2e33506dab70edc1eac29bf92516f5388b
Mike Shon <michael_shon@yahoo.com>
2026-02-28 15:12:36 -0500

    Update start.py- read and print app/.version

--- app/start.py
+++ app/start.py
@@ -1,4 +1,10 @@
 
 print("Hello World!")
+
+import os
+f = open("app/.version")
+version = f.read()
+print("version is " + version)
+
 import subdir.subdir
 print("subdir contains : " + subdir.subdir_var) 
