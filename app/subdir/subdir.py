84759780056cc460a7e14d810edc47924b786ffe
Mike Shon <michael_shon@yahoo.com>
2026-03-03 10:17:31 -0500

    Update subdir.py- comment to show how to import this file

--- app/subdir/subdir.py
+++ app/subdir/subdir.py
@@ -1,1 +1,6 @@
-subdir_var = "first thing"
+# to import app/subdir/subdir.py :
+# in  file in app/: import app.subdir.subdir
+# print("subdir contains : " + app.subdir.subdir.subdir_var) 
+
+
+subdir_var = "variable subdir_var with first thing"
